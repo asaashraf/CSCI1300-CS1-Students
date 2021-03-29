fc5fd0af7ebdb29217ebb7672c452eca93b2e9bd
asaashraf <60381989+asaashraf@users.noreply.github.com>
2021-03-29 12:40:46 -0600

    Create fib.cpp

---
+++ recursion/fib.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int fib(int x)
+{
+    if((x==1) || (x==0))
+    {
+        return x;
+    }
+    else
+    {
+        return (fib(x-1) + fib(x-2)); // 0 or 1
+    }
+}
+
+int main()
+{
+    int x, i=0;
+
+    cout << "enter the numer of terms of series: ";
+    cin >> x;
+
+    cout << fib(x);
+
+    return 0;
+}
